f6d4640adea64aa53a20cb3bcd72d1428c86da22
sehonge <sehong1602@naver.com>
2022-06-29 22:40:10 +0900

    WebSecurityConfigurerAdapter deprecated

--- src/main/kotlin/com/hong/jpastudy/config/WebSecurityConfig.kt
+++ src/main/kotlin/com/hong/jpastudy/config/WebSecurityConfig.kt
@@ -4,27 +4,55 @@ import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.security.config.annotation.web.builders.HttpSecurity
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter
+import org.springframework.security.config.http.SessionCreationPolicy
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.security.crypto.password.PasswordEncoder
+import org.springframework.security.web.SecurityFilterChain
 
 @Configuration
 @EnableWebSecurity
-open class WebSecurityConfig: WebSecurityConfigurerAdapter() {
+open class WebSecurityConfig {
 
     @Bean
-    open fun getPasswordEncoder(): PasswordEncoder {
+    open fun passwordEncoder(): PasswordEncoder {
         return BCryptPasswordEncoder()
     }
 
-
     // WebSecurityConfigurerAdapter deprecated -> https://spring.io/blog/2022/02/21/spring-security-without-the-websecurityconfigureradapter
-//    @Bean
-    override fun configure(http: HttpSecurity) {
+    @Bean
+    open fun filterChain(http: HttpSecurity): SecurityFilterChain {
         http
+            .authorizeHttpRequests{
+                authorize -> authorize.mvcMatchers()
+            }
+            .oauth2ResourceServer {
+                it.jwt {
+                    it.jwtAuthenticationConverter()
+                }
+            }
             .csrf().disable()
             .formLogin().disable()
             .headers().frameOptions().disable()
+            .and()
+            .exceptionHandling()
+            .and()
+            .sessionManagement()
+            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
 
     }
+
+//    override fun configure(http: HttpSecurity) {
+//        http
+//            .csrf().disable()
+//            .formLogin().disable()
+//            .headers().frameOptions().disable()
+//            .and()
+//            .exceptionHandling()
+//            .authenticationEntryPoint(unauth)
+//            .and()
+//            .sessionManagement()
+//            .sessionCreationPolicy(SessionCreationPolicy.STATELESS)
+//
+//
+//    }
 }
