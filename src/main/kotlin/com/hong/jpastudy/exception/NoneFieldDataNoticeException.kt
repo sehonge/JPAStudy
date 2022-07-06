87fd4bad36de2e424ed5980fe7b87517444f4c30
sehonge <sehong1602@naver.com>
2022-07-06 16:32:09 +0900

    UserNotFoundException 추가

--- src/main/kotlin/com/hong/jpastudy/exception/NoneFieldDataNoticeException.kt
+++ src/main/kotlin/com/hong/jpastudy/exception/NoneFieldDataNoticeException.kt
@@ -3,3 +3,4 @@ package com.hong.jpastudy.exception
 class NoneNoticeException(msg: String) : RuntimeException(msg)
 class NoticeConvertException(msg: String) : RuntimeException(msg)
 class NotValidUserForm(msg: String) : RuntimeException(msg)
+class UserNotFoundException(msg: String) : RuntimeException(msg)
